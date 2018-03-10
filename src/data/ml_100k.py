cc2c95c8e80860ed050137c230aa938cd53b73e2
yxtay <wyextay@gmail.com>
2018-03-10 14:50:37 +0800

    complete data download and preprocessing script

--- src/data/ml_100k.py
+++ src/data/ml_100k.py
@@ -9,17 +9,20 @@ import dask.dataframe as dd
 import requests
 
 from src.logger import get_logger
+from src.tf_utils import dd_tfrecord
 from src.utils import PROJECT_DIR, make_dirs
 
 logger = get_logger(__name__)
 
 FILES_CONFIG = {
-    "events": {"filename": "u.data", "sep": "\t", "columns": ["user_id", "item_id", "rating", "timestamp"]},
     "users": {"filename": "u.user", "sep": "|", "columns": ["user_id", "age", "gender", "occupation", "zipcode"]},
     "items": {"filename": "u.item", "sep": "|",
               "columns": ["item_id", "title", "release", "video_release", "imdb", "unknown", "action", "adventure",
                           "animation", "children", "comedy", "crime", "documentary", "drama", "fantasy", "filmnoir",
                           "horror", "musical", "mystery", "romance", "scifi", "thriller", "war", "western"]},
+    "all": {"filename": "u.data", "sep": "\t", "columns": ["user_id", "item_id", "rating", "timestamp"]},
+    "train": {"filename": "ua.base", "sep": "\t", "columns": ["user_id", "item_id", "rating", "timestamp"]},
+    "test": {"filename": "ua.test", "sep": "\t", "columns": ["user_id", "item_id", "rating", "timestamp"]},
 }
 
 
@@ -53,40 +56,56 @@ def load_data(src_dir: str = "data/ml-100k") -> Dict[str, dd.DataFrame]:
     return data
 
 
-def process_data(data: Dict[str, dd.DataFrame], save_path: str = "data/ml-100k/processed.csv") -> dd.DataFrame:
-    # process events
-    events = data["events"]
-    events["normalised_rating"] = events["rating"] / (events["rating"].max() - events["rating"].min())
-    events["datetime"] = dd.to_datetime(events["timestamp"], unit="s")
-    events["year"] = events["datetime"].dt.year
-    events["month"] = events["datetime"].dt.month
-    events["day"] = events["datetime"].dt.day
-    events["week"] = events["datetime"].dt.week
-    events["dayofweek"] = events["datetime"].dt.dayofweek
-    data["events"] = events
-
+def process_data(data: Dict[str, dd.DataFrame]) -> Dict[str, dd.DataFrame]:
     # process users
     users = data["users"]
     users["zipcode1"] = users["zipcode"].str.get(0)
     users["zipcode2"] = users["zipcode"].str.slice(0, 2)
     users["zipcode3"] = users["zipcode"].str.slice(0, 3)
+    data["users"] = users.persist()
+    logger.debug("users data processed.")
 
     # process items
     items = data["items"]
     items["release"] = dd.to_datetime(items["release"])
     items["release_year"] = items["release"].dt.year
+    data["items"] = items.persist()
+    logger.debug("items data processed.")
+
+    # process context
+    for el in ["all", "train", "test"]:
+        context = data[el]
+        context["normalised_rating"] = context["rating"] / (context["rating"].max() - context["rating"].min())
+        context["datetime"] = dd.to_datetime(context["timestamp"], unit="s")
+        context["year"] = context["datetime"].dt.year
+        context["month"] = context["datetime"].dt.month
+        context["day"] = context["datetime"].dt.day
+        context["week"] = context["datetime"].dt.week
+        context["dayofweek"] = context["datetime"].dt.dayofweek
+        data[el] = context
+    logger.debug("context data processed.")
 
     # merge data
-    df = (data["events"]
-          .merge(data["users"], "left", "user_id")
-          .merge(data["items"], "left", "item_id"))
+    dfs = {item: (data[item]
+                  .merge(data["users"], "left", "user_id")
+                  .merge(data["items"], "left", "item_id")
+                  .persist())
+           for item in ["all", "train", "test"]}
     logger.info("data merged.")
 
-    # save data
-    if save_path:
-        df.compute().to_csv(make_dirs(save_path, file=True), index=False)
+    return dfs
+
+
+def save_data(dfs: Dict[str, dd.DataFrame], save_dir: str = "data/ml-100k") -> None:
+    make_dirs(save_dir)
+
+    for name, df in dfs.items():
+        # save csv
+        save_path = str(Path(save_dir, name + ".csv"))
+        df.compute().to_csv(save_path, index=False)
         logger.info("data saved: %s.", save_path)
-    return df
+        # save tfrecord
+        dd_tfrecord(df, str(Path(save_dir, name + ".tfrecord")))
 
 
 if __name__ == "__main__":
@@ -104,9 +123,11 @@ if __name__ == "__main__":
     logger.debug("ArgumentParser: %s.", args)
 
     try:
+        data_dir = str(Path(args.dest, "ml-100k"))
         download_data(args.url, args.dest)
-        data = load_data(Path(args.dest, "ml-100k"))
-        process_data(data, Path(args.dest, "ml-100k", "processed.csv"))
+        data = load_data(data_dir)
+        dfs = process_data(data)
+        save_data(dfs, data_dir)
 
     except Exception as e:
         logger.exception(e)
---
+++ src/tf_utils.py
@@ -0,0 +1,62 @@
+import dask.dataframe as dd
+import numpy as np
+import tensorflow as tf
+
+from src.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+def dd_tfrecord(df: dd.DataFrame, tfrecord_path: str) -> None:
+    feature_func = {
+        np.int64: lambda value: tf.train.Feature(int64_list=tf.train.Int64List(value=[value])),
+        np.float64: lambda value: tf.train.Feature(float_list=tf.train.FloatList(value=[value])),
+        np.object_: lambda value: tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.compat.as_bytes(value)]))
+    }
+
+    # convert types to tf base types
+    df = df.copy()
+    for col in df.columns:
+        # convert non numeric types to string
+        if df[col].dtype.type not in {np.int64, np.float64}:
+            df[col] = df[col].astype(str)
+    logger.debug("data column types: %s.", list(df.dtypes.items()))
+
+    # configure feature specification base on column type
+    col_func = {col_name: feature_func[col_type.type]
+                for col_name, col_type in df.head().dtypes.items()}
+
+    # save tfrecord
+    with tf.python_io.TFRecordWriter(tfrecord_path) as writer:
+        for row in df.itertuples():
+            example = tf.train.Example(
+                features=tf.train.Features(
+                    feature={
+                        name: func(getattr(row, name))
+                        for name, func in col_func.items()
+                    }))
+            writer.write(example.SerializeToString())
+    logger.info("tfrecord saved: %s.", tfrecord_path)
+
+
+def tf_csv_dataset(csv_path: str, label_col: str, col_defaults: dict = {},
+                   shuffle: bool = False, batch_size: int = 32) -> tf.data.Dataset:
+    df = dd.read_csv(csv_path)
+    # use col_defaults if specified for col, else use defaults base on col type
+    type_defaults = {np.int64: 0, np.float64: 0.0, np.object_: ""}
+    record_defaults = [[col_defaults.get(col_name, type_defaults.get(col_type.type, ""))]
+                       for col_name, col_type in df.dtypes.items()]
+
+    def parse_csv(value):
+        columns = tf.decode_csv(value, record_defaults)
+        features = dict(zip(df.columns.tolist(), columns))
+        label = features[label_col]
+        return features, label
+
+    # read, parse, shuffle and batch dataset
+    dataset = tf.data.TextLineDataset(csv_path).skip(1)
+    if shuffle:
+        dataset = dataset.shuffle(buffer_size=1024)
+    dataset = dataset.map(parse_csv, num_parallel_calls=8)
+    dataset = dataset.batch(batch_size)
+    return dataset
